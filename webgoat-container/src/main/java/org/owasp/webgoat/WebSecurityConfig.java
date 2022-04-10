9fcc1d4c3e1f5245d0ecc1e16f79727e5c742a59
Marina <marinacuervanadal@outlook.es>
2022-04-10 12:08:27 +0200

    Update WebSecurityConfig.java
    Co-Authored-By: LuisVivianiB <81012836+luisVivianiB@users.noreply.github.com>
    Co-Authored-By: allbaa07 <102314470+allbaa07@users.noreply.github.com>
    Co-Authored-By: iratxeloyo <102314788+iratxeloyo@users.noreply.github.com>

--- webgoat-container/src/main/java/org/owasp/webgoat/WebSecurityConfig.java
+++ webgoat-container/src/main/java/org/owasp/webgoat/WebSecurityConfig.java
@@ -77,7 +77,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(null).passwordEncoder(bCryptPasswordEncoder());
+        auth.userDetailsService.passwordEncoder(bCryptPasswordEncoder(userDetailsService));
+
     }
 
     @Bean
