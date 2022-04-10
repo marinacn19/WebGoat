265c8b2b920bf382caf35df95af18fc83bf526f3
Marina <marinacuervanadal@outlook.es>
2022-04-10 17:45:57 +0200

    Update parsero
    Co-Authored-By: LuisVivianiB <81012836+luisVivianiB@users.noreply.github.com>
    Co-Authored-By: allbaa07 <102314470+allbaa07@users.noreply.github.com>
    Co-Authored-By: iratxeloyo <102314788+iratxeloyo@users.noreply.github.com>

--- webgoat-lessons/jwt/src/main/java/org/owasp/webgoat/jwt/JWTVotesEndpoint.java
+++ webgoat-lessons/jwt/src/main/java/org/owasp/webgoat/jwt/JWTVotesEndpoint.java
@@ -167,8 +167,7 @@ public class JWTVotesEndpoint extends AssignmentEndpoint {
             return failed(this).feedback("jwt-invalid-token").build();
         } else {
             try {
-                Claims claims = (Claims) Jwts.parser().setSigningKey(JWT_PASSWORD).parseClaimsJws(accessToken).getBody();
-                //Claims claims = (Claims) jwt.getBody();
+                Claims claims = Jwts.parser().setSigningKey(JWT_PASSWORD).parseClaimsJws(accessToken).getBody();
                 boolean isAdmin = Boolean.valueOf((String) claims.get("admin"));
                 if (!isAdmin) {
                     return failed(this).feedback("jwt-only-admin").build();
